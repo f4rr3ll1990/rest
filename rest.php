8578f10aff320ed686ebf0b16ea74b7d3e7dc89d
f4rr3ll90 <00mazahaka00@gmail.com>
2018-02-11 09:00:12 +0200

    error_reporting

--- rest.php
+++ rest.php
@@ -1,5 +1,5 @@
 <?php
- 
+error_reporting( E_ERROR );
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
